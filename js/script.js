c32f967b4b7d06546c4594967079d7771f8170f3
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-10-07 20:54:29 -0300

    #

---
+++ js/script.js
@@ -0,0 +1,9 @@
+storageFunctions = {
+        guardarLed: (valor) => localStorage.setItem("led", JSON.stringify(valor)),
+        pegarLed: () => JSON.parse(localStorage.getItem("led"))
+}
+
+function controlarLeds(valor){
+        storageFunctions.guardarLed(valor)
+        window.location.href = "../conexao.html"
+}
