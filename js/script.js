d19b7bb71b1e273c8448e39214e99e0f356d7fb0
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-10-08 15:20:25 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-storageFunctions = {
+let storageFunctions = {
         guardarLed: (valor) => localStorage.setItem("led", JSON.stringify(valor)),
         pegarLed: () => JSON.parse(localStorage.getItem("led"))
 }
@@ -6,23 +6,4 @@ storageFunctions = {
 function controlarLeds(valor){
         storageFunctions.guardarLed(valor)
         window.location.href = "./conexao.html"
-}
-
-
-let json = {
-        "error": false,
-        "category": "Programming",
-        "type": "single",
-        "joke": "A guy walks into a bar and asks for 1.4 root beers.\nThe bartender says \"I'll have to charge you extra, that's a root beer float\".\nThe guy says \"In that case, better make it a double.\"",
-        "flags": {
-            "nsfw": false,
-            "religious": false,
-            "political": false,
-            "racist": false,
-            "sexist": false,
-            "explicit": false
-        },
-        "id": 2,
-        "safe": true,
-        "lang": "en"
 }
