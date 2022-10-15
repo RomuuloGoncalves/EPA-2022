b4a5d41b8c38ccf94f5dba5e7f30f1457533a563
Pedro S <97417230+Pedro-S-Vicente@users.noreply.github.com>
2022-10-15 15:53:55 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,24 @@
-const containerLampadas = document.getElementById("lampadas")
-const containerGrupos = document.getElementById("grupos")
-
-let arrayLampadas = [
-    {
-        nome: "luz-1",
-        estado: 1
-    },
-
-    {
-        nome: "luz-2",
-        estado: 0
-    },   
-    
-    {
-        nome: "luz-3",
-        estado: 1
-    },
-    
-    {
-        nome: "luz-4",
-        estado: 0
-    },
-
-    {
-        nome: "luz-5",
-        estado: 0
-    },
-]
-
-let arrayGrupos = [
-    {
-        nome: "Sala",
-        estado: 1,
-        lampadas: [],
-    },
-
-    {
-        nome: "Cozinha",
-        estado: 0,
-        lampadas: [],
-    },
-
-    {
-        nome: "Varanda",
-        estado: 1,
-        lampadas: [],
-    },
-]
-
-function atualizarConteudoPagina(){
-    containerLampadas.innerHTML = ""
-    arrayLampadas.forEach(preencherLampadas)
-
-    containerGrupos.innerHTML = ""
-    arrayGrupos.forEach(preencherGrupos)
-}
-
-// Testes pra enviar informações pro arduíno né pae
-let storageFunctions = {
-    guardarLed: (valor) => localStorage.setItem("led", JSON.stringify(valor)),
-    pegarLed: () => JSON.parse(localStorage.getItem("led")),
-};
-
-function controlarLeds(valor) {
-    storageFunctions.guardarLed(valor);
-    window.location.href = "./conexao.html";
-}
-
-function diminuirOpacidade(){
-    selecionados = document.querySelectorAll("input[type='checkbox']:checked")
-
-    selecionados.forEach((elemento, indice)=>{
-        elemento.style
-    })
+function abrirModal (id) {
+    document.getElementById(id).classList.remove("apagar")
+    colocarBlur();
 }
 
 function fecharModal(id){
     document.getElementById(id).classList.add("apagar")
-    apagarBlur()
-    location.reload()
+    apagarBlur();
+}
+
+function colocarBlur () {
+    document.querySelectorAll(".page")[0].classList.remove("remover");
+    document.querySelectorAll(".page")[1].classList.remove("remover");
 }
 
 function apagarBlur(){
-    document.querySelectorAll(".page")[0].classList.add("remover")
-    document.querySelectorAll(".page")[1].classList.add("remover")
+    document.querySelectorAll(".page")[0].classList.add("remover");
+    document.querySelectorAll(".page")[1].classList.add("remover");
 }
 
 function toggleModal(id){
-    alert("oi")
-    modal = document.getElementById(id)
-    modal.classList.toggle("desativar")
+    modal = document.getElementById(id);
+    modal.classList.toggle("desativar");
 }
-
-atualizarConteudoPagina()
