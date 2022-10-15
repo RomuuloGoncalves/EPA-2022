2ee2ce5698e8e27e4b0ad06f2c1f076be3b28cf4
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-10-14 23:12:32 -0300

    #

--- cadastrar_grupo.php
+++ cadastrar_grupo.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +9,7 @@
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/style_cadastrar.css">
     <style>
-        body{
+        body {
             height: 100vh;
             width: 100vw;
             display: flex;
@@ -16,7 +17,7 @@
             align-items: center;
         }
 
-        .aviso{
+        .aviso {
             height: 50px;
             display: flex;
             justify-content: center;
@@ -26,74 +27,77 @@
 
 
         }
-        
-        #aviso__certo{            
+
+        #aviso__certo {
             color: #0f5132;
             background-color: #d1e7dd;
-            border-color: #badbcc;;
+            border-color: #badbcc;
+            ;
         }
 
-        #aviso__errado{
+        #aviso__errado {
             color: #842029;
             background-color: #f8d7da;
             border-color: #f5c2c7;
         }
     </style>
 </head>
+
 <body>
     <?php
-        require "./lib/conn.php";
+    require "./lib/conn.php";
 
-        // var_dump($_POST);
-        $erro = false;
-        if(empty($_POST) || !isset($_POST)){
-            $erro = "Preencha os campos necessários";
+    // var_dump($_POST);
+    $erro = false;
+    if (empty($_POST) || !isset($_POST)) {
+        $erro = "Preencha os campos necessários";
+    }
+
+    if (empty($_POST["nome"])) {
+        $erro = "Preencha o campo 'nome'";
+    }
+
+    if (!$erro) {
+        if (empty($_POST["lampadas_selecionadas"]) || !isset($_POST["lampadas_selecionadas"])) {
+            $erro .= "É necessário escolher as lampadas para este grupo";
+        } else {
+            $lampadas_selecionadas = array_filter($_POST["lampadas_selecionadas"]);
         }
+    }
 
-        if(empty($_POST["nome"])){
-            $erro = "Preencha o campo 'nome'";
-        }
+    if (!$erro) {
+        $sql_cadastrar = "INSERT INTO GRUPOS VALUES(0, :nome)";
+        $stmt = $conn->prepare($sql_cadastrar);
+        $stmt->bindValue(":nome", $_POST["nome"]);
+        $stmt->execute();
 
-        if(!$erro){
-            if(empty($_POST["lampadas_selecionadas"]) || !isset($_POST["lampadas_selecionadas"])){
-                $erro .= "É necessário escolher as lampadas para este grupo";
-            }else{
-                $lampadas_selecionadas = array_filter($_POST["lampadas_selecionadas"]);
-            }
-        }
+        $sql_pegar_id = "SELECT ID_GRUPO FROM GRUPOS ORDER BY ID_GRUPO DESC LIMIT 1";
+        $stmt = $conn->query($sql_pegar_id);
+        $id_grupo = $stmt->fetch(PDO::FETCH_OBJ);
 
-        if(!$erro){
-            $sql_cadastrar = "INSERT INTO GRUPOS VALUES(0, :nome)";
+        foreach ($lampadas_selecionadas as $lampada) {
+            // var_dump($id_grupo);
+            $sql_cadastrar = "INSERT INTO LAMPADAS_GRUPO VALUES($id_grupo->ID_GRUPO, :id_lampada)";
             $stmt = $conn->prepare($sql_cadastrar);
-            $stmt -> bindValue(":nome", $_POST["nome"]);
-            $stmt -> execute();
-
-            $sql_pegar_id = "SELECT ID_GRUPO FROM GRUPOS ORDER BY ID_GRUPO DESC LIMIT 1";
-            $stmt = $conn -> query($sql_pegar_id);
-            $id_grupo = $stmt -> fetch(PDO::FETCH_OBJ);
-            
-            foreach($lampadas_selecionadas as $lampada){
-                // var_dump($id_grupo);
-                $sql_cadastrar = "INSERT INTO LAMPADAS_GRUPO VALUES($id_grupo->ID_GRUPO, :id_lampada)";
-                $stmt = $conn->prepare($sql_cadastrar);
-                $stmt -> bindValue(":id_lampada", $lampada);
-                $stmt -> execute();
-            }
-
-            ?>
-                <div class="aviso" id="aviso__certo">
-                    Cadastrado com sucesso!!!
-                </div>
-                <meta http-equiv="refresh" content="3; url=./index.php">
-            <?php
-        }else{
-            ?>
-            <div class="aviso" id="aviso__errado">
-                    <?= $erro ?>
-            </div>
-            <meta http-equiv="refresh" content="3; url=./cadastro_grupos.php">
-        <?php
+            $stmt->bindValue(":id_lampada", $lampada);
+            $stmt->execute();
         }
-        ?>
+
+    ?>
+        <div class="aviso" id="aviso__certo">
+            Cadastrado com sucesso!!!
+        </div>
+        <meta http-equiv="refresh" content="3; url=./index.php">
+    <?php
+    } else {
+    ?>
+        <div class="aviso" id="aviso__errado">
+            <?= $erro ?>
+        </div>
+        <meta http-equiv="refresh" content="3; url=./cadastro_grupos.php">
+    <?php
+    }
+    ?>
 </body>
+
 </html>
