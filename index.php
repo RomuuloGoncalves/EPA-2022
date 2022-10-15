38e8a79922c5824784158f2a7415830ff1ce2b1c
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-10-15 16:17:18 -0300

    #

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-	require "./lib/conn.php";
-	require "./functions/atualizar_json.php";
+require "./lib/conn.php";
+require "./functions/atualizar_json.php";
 
-	atualizar_json('SELECT PORTA, ESTADO FROM LAMPADAS', './json/lampadas.json');
-	$selectGrupos = 'SELECT * FROM GRUPOS';
-	$stmt = $conn->query($selectGrupos);
-	$grupos = $stmt->fetchAll(PDO::FETCH_OBJ);
+atualizar_json('SELECT PORTA, ESTADO FROM LAMPADAS', './json/lampadas.json');
+$selectGrupos = 'SELECT * FROM GRUPOS';
+$stmt = $conn->query($selectGrupos);
+$grupos = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-	$selectLampadas = 'SELECT * FROM LAMPADAS';
-	$stmt = $conn->query($selectLampadas);
-	$lampadas = $stmt->fetchAll(PDO::FETCH_OBJ);
+$selectLampadas = 'SELECT * FROM LAMPADAS';
+$stmt = $conn->query($selectLampadas);
+$lampadas = $stmt->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <!DOCTYPE html>
@@ -48,18 +48,18 @@
 
 			<div class="slide" id="lampadas">
 				<?php
-					foreach($lampadas as $lampada) {
-						?>
-						<div class="card card__lampada">
-							<a href="functions/mudar_estado_lampada.php?id=<?=$lampada->ID_LAMPADA?>&est=<?=$lampada->ESTADO?>">
-								<img src="./assets/img/lampada_<?=$lampada->ESTADO?>.png" alt="Lampada <?=$lampada->ESTADO?>" />
-							</a>
-							<a href="gerenciar_lampada.php?id=<?=$lampada->ID_LAMPADA?>">
-								<p><?=$lampada->NOME?></p>
-							</a>
-						</div>
-						<?php
-					}
+				foreach ($lampadas as $lampada) {
+				?>
+					<div class="card card__lampada">
+						<a href="functions/mudar_estado_lampada.php?id=<?= $lampada->ID_LAMPADA ?>&est=<?= $lampada->ESTADO ?>">
+							<img src="./assets/img/lampada_<?= $lampada->ESTADO ?>.png" alt="Lampada <?= $lampada->ESTADO ?>" />
+						</a>
+						<a href="gerenciar_lampada.php?id=<?= $lampada->ID_LAMPADA ?>">
+							<p><?= $lampada->NOME ?></p>
+						</a>
+					</div>
+				<?php
+				}
 				?>
 			</div>
 
@@ -77,50 +77,51 @@
 
 			<div class="slide" id="grupos">
 				<?php
-					foreach($grupos as $grupo) {
-						$select_lampadas_acessas = "SELECT * FROM LAMPADAS_GRUPO
+				foreach ($grupos as $grupo) {
+					$select_lampadas_acessas = "SELECT * FROM LAMPADAS_GRUPO
 						INNER JOIN LAMPADAS
 						ON LAMPADAS.ID_LAMPADA = LAMPADAS_GRUPO.ID_LAMPADA
 						WHERE LAMPADAS.ESTADO = 1
 						AND LAMPADAS_GRUPO.ID_GRUPO = :id_grupo";
-						$lampadas_acessas = $conn->prepare($select_lampadas_acessas);
-						$lampadas_acessas->bindValue(':id_grupo', $grupo->ID_GRUPO);
-						$lampadas_acessas->execute();
-						$qtdd_lampadas_acessas = $lampadas_acessas->rowCount();
-						$select_lampadas = "SELECT * FROM LAMPADAS_GRUPO
+					$lampadas_acessas = $conn->prepare($select_lampadas_acessas);
+					$lampadas_acessas->bindValue(':id_grupo', $grupo->ID_GRUPO);
+					$lampadas_acessas->execute();
+					$qtdd_lampadas_acessas = $lampadas_acessas->rowCount();
+					$select_lampadas = "SELECT * FROM LAMPADAS_GRUPO
 
 						WHERE LAMPADAS_GRUPO.ID_GRUPO = :id_grupo";
-						$lampadas = $conn->prepare($select_lampadas);
-						$lampadas->bindValue(':id_grupo', $grupo->ID_GRUPO);
-						$lampadas->execute();
-						$qtdd_lampadas = $lampadas->rowCount();
+					$lampadas = $conn->prepare($select_lampadas);
+					$lampadas->bindValue(':id_grupo', $grupo->ID_GRUPO);
+					$lampadas->execute();
+					$qtdd_lampadas = $lampadas->rowCount();
 
-						if(isset($_POST['checkbox']) || $qtdd_lampadas ===  $qtdd_lampadas_acessas) {
-							$estado_comodo = 1;
-							$estado_switch = "checked";
-						} else {
-							$estado_comodo = 0;
-							$estado_switch = "";
-						}
-					?>
-						<div class="card card__grupo">
-							<img src="./assets/img/lampada_<?=$estado_comodo?>.png" alt="Lampada <?=$estado_comodo?>"/>
-
-								<label for="checkbox-<?=$grupo->ID_GRUPO?>" class="switch">
-									<a href="functions/mudar_estado_grp.php?id=<?=$grupo->ID_GRUPO?>&est=<?=($estado_comodo===1)?0:1?>">
-										<input type="checkbox" name="checkbox" id="checkbox-<?=$grupo->ID_GRUPO?>" <?=$estado_switch?>/>
-										<span class="slider"></span>
-									</a>
-								</label>
-
-							<div class="page__titulo">
-								<p><?=$grupo->NOME?></p>
-									<ion-icon name="information-circle-outline"></ion-icon>
-								</a>
-							</div>
-						</div>
-						<?php
+					if (isset($_POST['checkbox']) || $qtdd_lampadas ===  $qtdd_lampadas_acessas) {
+						$estado_comodo = 1;
+						$estado_switch = "checked";
+					} else {
+						$estado_comodo = 0;
+						$estado_switch = "";
 					}
+				?>
+					<div class="card card__grupo">
+						<img src="./assets/img/lampada_<?= $estado_comodo ?>.png" alt="Lampada <?= $estado_comodo ?>" />
+
+						<label for="checkbox-<?= $grupo->ID_GRUPO ?>" class="switch">
+							<a href="functions/mudar_estado_grp.php?id=<?= $grupo->ID_GRUPO ?>&est=<?= ($estado_comodo === 1) ? 0 : 1 ?>">
+								<input type="checkbox" name="checkbox" id="checkbox-<?= $grupo->ID_GRUPO ?>" <?= $estado_switch ?> />
+								<span class="slider"></span>
+							</a>
+						</label>
+
+						<div class="page__titulo">
+							<p><?= $grupo->NOME ?></p>
+							<a href="gerenciar_grupo.php?id=<?= $grupo->ID_GRUPO ?>">
+								<ion-icon name="information-circle-outline"></ion-icon>
+							</a>
+						</div>
+					</div>
+				<?php
+				}
 				?>
 			</div>
 		</div>
